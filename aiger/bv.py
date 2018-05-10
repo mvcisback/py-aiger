5a7347a5ebcca94917339194d584ac4d65e731ae
Markus N Rabe <markus.norman.rabe@gmail.com>
2018-05-10 14:24:19 -0700

    first working version of bitvector library

--- aiger/bv.py
+++ aiger/bv.py
@@ -1,19 +1,18 @@
 
 import common
+from common import AAG
 import parser
-
+import aiger  # needed only for the isinstance(kind[1], aiger.common.AAG) check
 
 def signal(name):
     return common.and_gate([name], output=name)
 
 def rename_output(wordlen, aig, cur_name='x', new_name='y'):
     return aig['o', {cur_name + '[{}]'.format(i): new_name + '[{}]'.format(i) for i in range(wordlen)}]
-
 # print(rename_output(2, word(2)))
 
 def rename_input(wordlen, aig, cur_name='x', new_name='y'):
     return aig['i', {cur_name + '[{}]'.format(i): new_name + '[{}]'.format(i) for i in range(wordlen)}]
-
 # print(rename_input(2, word(2)))
 
 def bv_and(wordlen, output='x&y', left='x', right='y'):
@@ -27,7 +26,6 @@ def bv_or(wordlen, output='x&y', left='x', right='y'):
     for i in range(wordlen):
         aig = aig | common.or_gate([left + '[{}]'.format(i), right + '[{}]'.format(i)], output=output + '[{}]'.format(i))
     return aig
-
 # print(bv_or(2))
 
 def const(wordlen, value, output='x'):
@@ -38,17 +36,12 @@ def const(wordlen, value, output='x'):
         value = value // 2
     return aig
 
-# print(const(3, 2))
-# print(const(3, 8))
-
-
 def _full_adder(x, y, carry_in, result, carry_out):
     return parser.parse("aag 10 3 0 2 7\n2\n4\n6\n18\n21\n8 4 2\n10 5 3\n12 11 9\n14 12 6\n16 13 7\n18 17 15\n20 15 9\ni0 {}\ni1 {}\ni2 {}\no0 {}\no1 {}\n".format(x, y, carry_in, result, carry_out))
 # print(full_adder('x', 'y', 'carry_in', 'x+y', 'carry_out'))
 
 
-
-def adder(wordlen, output='x+y', left='x', right='y'):
+def _adder_circuit(wordlen, output='x+y', left='x', right='y'):
     carry_name = output + '_carry'
     aig = common.source({carry_name: False})
     assert left != carry_name and right != carry_name
@@ -56,29 +49,22 @@ def adder(wordlen, output='x+y', left='x', right='y'):
         fa = _full_adder(left + "[{}]".format(i), right + "[{}]".format(i), carry_name, output+'[{}]'.format(i), carry_name)
         aig = aig >> fa
     return aig
+# print(_adder_circuit(4))
 
-# print(adder(4))
+def _incrementer_circuit(wordlen, output='x+1', input='x'):
+    return const(wordlen, 1, output='y') >> _adder_circuit(wordlen, output=output, left=input, right='y')
+# print(_incrementer_circuit(3))
 
-def inc(wordlen, output='x+1', input='x'):
-    return const(wordlen, 1, output='y') >> adder(wordlen, output=output, left=input, right='y')
-
-# print(inc(3))
-
-def negation(wordlen, output='not x', input='x'):
+def _negation_circuit(wordlen, output='not x', input='x'):
     # word(word) >> 
     return common.bit_flipper(inputs=[input + '[{}]'.format(i) for i in range(wordlen)],
                               outputs=[output + '[{}]'.format(i) for i in range(wordlen)])
+# print(_negation_circuit(2))
 
-# print(negation(2))
-
-def negative(wordlen, output='-x', input='x'):
+def _negative_circuit(wordlen, output='-x', input='x'):
     """Returns the circuit computing x*(-1) in Two's complement"""
-    return negation(wordlen, output='tmp', input=input) >> inc(wordlen, output=output, input='tmp')
-
-# print(negative(4))
-
-def subtraction(wordlen, output='x-y', left='x', right='y'):
-    return negation(wordlen, input=right, output='-'+right) >> adder(wordlen, output=output, left=left, right='-'+right)
+    return _negation_circuit(wordlen, output='tmp', input=input) >> _incrementer_circuit(wordlen, output=output, input='tmp')
+# print(_negative_circuit(4))
 
 
 
@@ -87,61 +73,129 @@ class BV:
 
         self.size = size
         self.variables = []
-
-        out_name = 'bv{}'.format(size)  # all BVs use this name for outputs
+        
+        if self.size == 0:
+            self.aig = common.empty()
+            return
 
         if isinstance(kind, int):  # Constant
-            self.aig = const(size, kind, output=out_name)
+            self.aig = const(size, abs(kind), output=self.name())
+            if kind < 0:
+                neg = - self
+                self.aig = neg.aig
         if isinstance(kind, str):  # Variable
             self.variables.append(kind)
             self.aig = common.empty()
             for i in range(self.size):
-                self.aig = self.aig | signal(kind + '[{}]'.format(i))
+                self.aig = self.aig | common.and_gate([kind + '[{}]'.format(i)], output=self.name(i))
         if isinstance(kind, tuple):  # for internal use only
             assert isinstance(kind[0], list)  # variables
-            self.variables = kind[0]
-            assert isinstance(kind[1], common.AAG)
-            self.aig = kind[1]            
+            self.variables.extend(kind[0])
+            assert isinstance(kind[1], common.AAG) or isinstance(kind[1], aiger.common.AAG)
+            self.aig = kind[1]
         
         assert len(self.aig.outputs) == self.size
-        assert len(self.aig.inputs) == self.size*len(self.variables)
+        # assert len(self.aig.inputs) == self.size*len(self.variables)
 
-    def name(self):
-        return "bv{}".format(self.size)
+    def __len__(self):
+        return self.size
 
     def __repr__(self):
-        return self.name() + ":\n" + str(self.aig)
+        return str(self.aig)
+
+    def name(self, idx=None):
+        if idx is None:
+            return "bv{}".format(self.size)
+        else:
+            return "bv{}[{}]".format(self.size, idx)
 
     def rename(self, name):
         """Renames the output of the expression; mostly used internally"""
-        new_aig = self.aig['o', {self.name() + '[{}]'.format(i): name + '[{}]'.format(i) for i in range(self.size)}]
-        return BV(self.size, (self.variables.copy(), new_aig))
+        new_aig = self.aig['o', {self.name(i): name + '[{}]'.format(i) for i in range(self.size)}]
+        return BV(self.size, (self.variables, new_aig))
 
     def subtitute(self, subst):  # TODO: test this function; does the simultaneous thing work?
         """Simultaniously substitutes one set of input words by another"""
         d = dict()
         for old, new in subst:
             d.update({old + '[{}]'.format(i): new + '[{}]'.format(i) for i in range(self.size)}) 
-        return BV(self.size, (self.variables.copy(), self.aig['i', d]))
+        return BV(self.size, (self.variables, self.aig['i', d]))
+
+    def assign(self, assignment):
+        """assignment must be map from names to integer values"""
+        aig = self.aig
+        names = assignment.keys()
+        for name, value in assignment.items():
+            c = const(self.size, value, output=name)
+            aig = c >> aig
+        variables = list(filter(lambda n: n not in names, self.variables))
+        return BV(self.size, (variables, aig))
 
     def __add__(self, other):
         assert self.size == other.size
-        other = other.rename("other")
-        return BV(self.size, 
-                    (self.variables + other.variables, 
-                     (self.aig | other.aig)
-                        >> (adder(self.size, output=self.name(), left=self.name(), right='other') >> common.sink([self.name() + '_carry']))
-                    ))
+        other = other.rename('other')
+        adder = _adder_circuit(self.size, output=self.name(), left=self.name(), right='other')  >>  common.sink([self.name() + '_carry'])
+        return BV(self.size, (self.variables + other.variables, self.aig >> (other.aig >> adder)))
 
-    def __len__(self):
-        return self.size
+    def __invert__(self):  # ~x
+        neg = _negation_circuit(self.size, output=self.name(), input=self.name())
+        return BV(self.size, (self.variables, self.aig >> neg))
+
+    def __neg__(self):  #-x
+        # aig = self.aig >> _negation_circuit(wordlen, input=self.name(), output=self.name())
+        # return BV(self.size, (self.variables, aig))
+        return ~self + BV(self.size, 1)
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __getitem__(self, k):
+        if isinstance(k, int):
+            out_idxs = [k]
+        else:
+            out_idxs = range(self.size).__getitem__(k)
+        out_map = dict(zip(out_idxs, range(len(out_idxs))))  # maps indices to their new position
+        if len(out_idxs) == 0:
+            return BV(0, 0)
+        outputs_to_remove = []
+        rename = dict()
+        for i in range(self.size):
+            if i in out_map:
+                new_idx = out_map[i]
+                rename.update({self.name(i): "bv{}[{}]".format(len(out_idxs), new_idx)})
+            else: 
+                outputs_to_remove.append(self.name(i))
+        aig = self.aig
+        if len(outputs_to_remove) > 0:
+            aig = aig >> common.sink(outputs_to_remove)
+        aig = aig['o', rename]
+        return BV(len(out_idxs), (self.variables, aig))
+
+    def reverse(self):
+        return self[::-1]
+
+    def concat(self, other):
+        self_rename = dict()
+        for i in range(self.size):
+            self_rename.update({self.name(i): "bv{}[{}]".format(self.size+other.size, i)})
+
+        other_rename = dict()
+        for i in range(other.size):
+            other_rename.update({other.name(i): "bv{}[{}]".format(self.size+other.size, self.size + i)})
+        return BV(self.size + other.size, (self.variables + other.variables, self.aig['o', self_rename] | other.aig['o', other_rename])) 
+
+    # Bitwise opeators
+    # def __rshift__(self, other):
+    # def __lshift__(self, other):
+    # def __or__(self, other):
+    # def __and__(self, other):
+    # def __xor__(self, other):
 
     # TODO
     # Arithmetic operations
     # def __neg__(self):
     # def __pos__(self):
         # return self
-    # def __sub__(self, other):
     # def __abs__(self):
 
     # Difficult arithmetic operations
@@ -159,38 +213,10 @@ class BV:
     # def __ge__(self, other):
 
     # def __hash__(self):  # for use in strash; remember hash with every expression to avoid recomputation; remember global map from hashes to subexpressions
-    
-    # Bitwise opeators
-    # def __getitem__(self, k):  # element access, and slice exctraction
-    # def __invert__(self):  # ~x
-    # def __rshift__(self, other):
-    # def __lshift__(self, other):
-    # def __or__(self, other):
-    # def __and__(self, other):
-    # def __xor__(self, other):
 
 
-print(BV(4, 6) + BV(4, 3))
 
 
-# self + other    __add__(self, other)
-# self - other    __sub__(self, other)
-# self * other    __mul__(self, other)
-# self / other    __div__(self, other) or __truediv__(self,other) if __future__.division is active.
-# self // other   __floordiv__(self, other)
-# self % other    __mod__(self, other)
-# divmod(self,other)  __divmod__(self, other)
-# self ** other   __pow__(self, other)
-# self & other    __and__(self, other)
-# self ^ other    __xor__(self, other)
-# self | other    __or__(self, other)
-# self << other   __lshift__(self, other)
-# self >> other   __rshift__(self, other)
-# bool(self)  __nonzero__(self) (used in boolean testing)
-# -self   __neg__(self)
-# +self   __pos__(self)
-# abs(self)   __abs__(self)
-# ~self   __invert__(self) (bitwise)
 # self += other   __iadd__(self, other)
 # self -= other   __isub__(self, other)
 # self *= other   __imul__(self, other)
--- aiger/calc.py
+++ aiger/calc.py
@@ -4,18 +4,18 @@ import parser
 import bv
 
 import tempfile
-from subprocess import call
+from subprocess import call, PIPE
 import re
 
 
-def simplify(aig):
+def simplify(expr):
     f = tempfile.NamedTemporaryFile()
-    f.write(str(aig).encode())
+    f.write(str(expr).encode())
     f.seek(0)
 
     call(["cp", f.name, f.name + ".aag"])
     call(["aigtoaig", f.name + ".aag", f.name + ".aig"])
-    call(["abc", "-c", "read {}; print_stats; dc2; dc2; dc2; print_stats; write {}".format(f.name + ".aig", f.name + ".aig")])
+    call(["abc", "-c", "read {}; print_stats; dc2; dc2; dc2; print_stats; write {}".format(f.name + ".aig", f.name + ".aig")], stdout=PIPE)
     call(["aigtoaig", f.name + ".aig", f.name + ".aag"])
 
     simplified = open(f.name + ".aag")
@@ -24,50 +24,32 @@ def simplify(aig):
     f.close()
     aig = parser.parse(simp_aig_string)
     aig.comments.pop()  # remove ABC's comments
-    return aig
+    return bv.BV(expr.size, (expr.variables, aig))
 
 
 def _bit_value(expr, name):
-    signal = expr.outputs[name]
+    signal = expr.aig.outputs[name]
     if signal not in [True, False]:  # relying on the fact that True and False are represented as 1 and 0
         raise ValueError('Value of {} not constant'.format(name))
     return signal
 
 
-def _bv_base_name(expr):
-    """Assumes that the expression is an AIG that has a single output word"""
-    name = list(expr.outputs.keys())[0]
-    m = re.search(r"(.*)\[[0-9_]+\]", name)  # match everything before the brackets '[i]'
-    return m.group(1) # TODO: may fail if not matched anything
-
-
-def value(wordlen, expr):
-    """Assumes that the expression is an AIG that has a single output word"""
-    name = _bv_base_name(expr)
+def unsigned_value(expr):
+    """Assumes that the expression is a BV"""
+    expr = simplify(expr)
     value = 0
-    for i in range(len(expr.outputs)):
-        value += 2**i * _bit_value(expr, name + '[{}]'.format(i))
+    for i in range(expr.size):
+        value += 2**i * _bit_value(expr, expr.name(i))
     return value
 
-assert value(4, bv.const(4, 6)) == 6
 
-def signed_value(wordlen, expr):
+def value(expr):
     """Assumes that the expression is an AIG that has a single output word"""
-    sign_bit_name = _bv_base_name(expr) + '[{}]'.format(str(wordlen - 1))
+    expr = simplify(expr)
+    sign_bit_name = expr.name() + '[{}]'.format(str(expr.size - 1))
     if not _bit_value(expr, sign_bit_name):
-        return value(expr)  # positive number
+        return unsigned_value(expr)  # positive number
     else:
-        return - value(simplify(expr >> bv.negative(4)))
+        return - unsigned_value(-expr)
 
-minus_six = bv.const(4, 6) >> bv.negative(4)
-print("Minus 6:", minus_six)
-print(signed_value(4, simplify(minus_six)))
 
-def assign(wordlen, expr, assignment={}):
-    for name, value in assignment:
-        expr = bv.const(wordlen) >> expr
-    return expr
-
-def eval(wordlen, expr, assignment={}):
-    assign(wordlen, expr, assignment)
-    return value(simplify(expr))
