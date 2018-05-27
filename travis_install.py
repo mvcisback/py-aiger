e20c4c0d296a25820fe8d9fe53050963acfe89ae
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-05-27 12:51:05 -0700

    yapf travis_install

--- travis_install.py
+++ travis_install.py
@@ -7,16 +7,15 @@ import tarfile
 import zipfile
 from pathlib import Path
 
-
 GET_ABC_CMD = "wget https://github.com/berkeley-abc/abc/archive/master.zip"
 GET_AIGER_CMD = "wget http://fmv.jku.at/aiger/aiger-1.9.9.tar.gz"
 
 EXTRACT_ABC_CMD = "unzip master.zip -d /tmp/abc"
 EXTRACT_AIGER_CMD = "tar -xvf {}/aiger-1.9.9.tar.gz"
 
-
 INSTALL_ABC_CMD = "cmake . && make"
 INSTALL_AIGER_CMD = "./configure.sh && make"
+
 
 # https://stackoverflow.com/questions/41742317/how-can-i-change-directory-with-python-pathlib
 @contextlib.contextmanager
@@ -49,7 +48,6 @@ def install_abc(abc_path):
 
         with zipfile.ZipFile(abc_path / "master.zip", "r") as f:
             f.extractall()
-
 
     with working_directory(abc_path / 'abc-master'):
         subprocess.check_call(INSTALL_ABC_CMD, shell=True)
