2350e761b6231d34b9fd791b7915ed56f3535a0a
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-06-08 18:45:41 -0700

    rewrite parsing and conversion tests to only check i/o correctness

--- aiger/test_aig.py
+++ aiger/test_aig.py
@@ -1,10 +1,14 @@
+import hypothesis.strategies as st
 from hypothesis import given
 
 from aiger import hypothesis as aigh
 
 
-@given(aigh.Circuits)
-def test_aig_to_aag(circ):
-    aag = circ._to_aag()
-    assert repr(aag) == repr(aag._to_aig())
+@given(aigh.Circuits, st.data())
+def test_aig_to_aag(circ, data):
+    circ2 = circ._to_aag()._to_aig()
+    assert circ.inputs == circ2.inputs
+    assert circ.outputs == circ2.outputs
+    test_input = {f'{i}': data.draw(st.booleans()) for i in circ.inputs}
+    assert circ(test_input) == circ2(test_input)
     
--- aiger/test_parser.py
+++ aiger/test_parser.py
@@ -1,5 +1,6 @@
 from tempfile import NamedTemporaryFile
 
+import hypothesis.strategies as st
 from hypothesis import given
 from lenses import bind
 
@@ -7,15 +8,23 @@ from aiger import hypothesis as aigh
 from aiger import parser as aigp
 
 
-@given(aigh.Circuits)
-def test_load(aag):
+@given(aigh.Circuits, st.data())
+def test_load(circ, data):
     with NamedTemporaryFile() as f:
-        aag.write(f.name)
-        aag2 = aigp.load(f.name)
+        circ.write(f.name)
+        circ2 = aigp.load(f.name)
+        
+    assert circ.inputs == circ2.inputs
+    assert circ.outputs == circ2.outputs
+    test_input = {f'{i}': data.draw(st.booleans()) for i in circ.inputs}
+    assert circ(test_input) == circ2(test_input)
 
-    assert repr(aag) == repr(aag2)
 
+@given(aigh.Circuits, st.data())
+def test_parse(circ, data):
+    circ2 = aigp.parse(repr(circ))
 
-@given(aigh.Circuits)
-def test_parse(aag):
-    assert aigp.parse(repr(aag)) == aag
+    assert circ.inputs == circ2.inputs
+    assert circ.outputs == circ2.outputs
+    test_input = {f'{i}': data.draw(st.booleans()) for i in circ.inputs}
+    assert circ(test_input) == circ2(test_input)
