89f5c61b1a035f134bb34333dbb14bc911d79321
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-06-21 14:49:51 -0700

    smoke check that simplify works

--- aiger/test_aig.py
+++ aiger/test_aig.py
@@ -1,6 +1,8 @@
 import hypothesis.strategies as st
 from hypothesis import given
 
+from aiger import bv
+from aiger import bv_utils
 from aiger import hypothesis as aigh
 
 
@@ -11,3 +13,13 @@ def test_aig_to_aag(circ, data):
     assert circ.outputs == circ2.outputs
     test_input = {f'{i}': data.draw(st.booleans()) for i in circ.inputs}
     assert circ(test_input) == circ2(test_input)
+
+
+def test_aig_to_aag2():
+    expr = abs(bv.BV(2, 'x'))
+    circ = expr.aig
+    circ2 = circ._to_aag()._to_aig()
+    assert circ.inputs == circ2.inputs
+    assert circ.outputs == circ2.outputs
+
+    bv_utils.simplify(expr)  # smoke test
