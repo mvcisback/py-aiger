ee2ba6c0e4691c5edd8ed2f3d2e4ea7d027fab17
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-07-19 16:28:34 -0700

    Implement feedback for gridworld example

--- examples/gridworld.py
+++ examples/gridworld.py
@@ -1,17 +1,29 @@
+from functools import reduce
+import operator as op
+
+
 import aiger as aig
 from aiger.bv import BV
 
 
-def gridworld1d(n, state_name, a0, a1, initials=None):
+def decode_start(num, n):
+    return [x == '1' for x in format(num, f'0{n}b')[::-1]]
+
+
+def _gridworld1d(n, state_name='x', start=0):
     # Create feedback adder AIG.
+    a0, a1 = f'a{state_name}0', f'a{state_name}1'
     state_bv = BV(n, state_name, state_name)
     delta_bv = BV(n, "delta", "delta")
-    adder_bv = state_bv + delta_bv
+    adder_bv = (state_bv + delta_bv).rename(state_name)
 
+    start = decode_start(start, n)
+    
     adder_aig = adder_bv.aig.feedback(
         inputs=state_bv.names,
         outputs=adder_bv.names,
-        initials=initials,
+        initials=start,
+        keep_outputs=True,
     )
 
     # Create action mux.
@@ -23,5 +35,8 @@ def gridworld1d(n, state_name, a0, a1, initials=None):
     return amux >> adder_aig
 
 
-if __name__ == '__main__':
-    g = gridworld1d(2, 'x', 'ax0', 'ax1')
+def gridworld2d(n, starts=None):
+    xworld = _gridworld1d(n, 'x', starts[0])
+    yworld = _gridworld1d(n, 'y', starts[1])
+    return xworld | yworld
+
