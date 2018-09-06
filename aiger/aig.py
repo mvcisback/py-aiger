a1d51b8ccdb3bad5517293c139d662d325e147a7
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-09-05 17:54:46 -0700

    remove noisy comment propogation + allow parallel composition to share inputs

--- aiger/aig.py
+++ aiger/aig.py
@@ -118,6 +118,9 @@ class AIG:
     def __rshift__(self, other):
         return seq_compose(self, other)
 
+    def __lshift__(self, other):
+        return seq_compose(other, self)
+
     def __or__(self, other):
         return par_compose(self, other)
 
@@ -265,17 +268,29 @@ class AIG:
 
 
 def par_compose(aig1, aig2, check_precondition=True):
-    if check_precondition:
-        assert not (aig1.latches & aig2.latches)
-        assert not (aig1.outputs & aig2.outputs)
+    assert not aig1.latches & aig2.latches
+    assert not aig1.outputs & aig2.outputs
 
-    return AIG(
+    shared_inputs = aig1.inputs & aig2.inputs
+    if shared_inputs:
+        relabels1 = {n: cmn._fresh() for n in shared_inputs}
+        relabels2 = {n: cmn._fresh() for n in shared_inputs}
+        aig1, aig2 = aig1['i', relabels1], aig2['i', relabels2]
+
+    circ = AIG(
         inputs=aig1.inputs | aig2.inputs,
         latch_map=aig1.latch_map | aig2.latch_map,
         latch2init=aig1.latch2init | aig2.latch2init,
         node_map=aig1.node_map | aig2.node_map,
-        comments=aig1.comments + ('|', ) + aig2.comments
+        comments=aig1.comments + aig2.comments
     )
+
+    if shared_inputs:
+        for orig in shared_inputs:
+            new1, new2 = relabels1[orig], relabels2[orig]
+            circ <<= cmn.tee({orig: [new1, new2]})
+
+    return circ
 
 
 def seq_compose(circ1, circ2, *, input_kinds=(Input,)):
@@ -297,5 +312,5 @@ def seq_compose(circ1, circ2, *, input_kinds=(Input,)):
         latch_map=circ1.latch_map | circ3.latch_map,
         latch2init=circ1.latch2init | circ2.latch2init,
         node_map=circ3.node_map | passthrough,
-        comments=circ1.comments + ('>>', ) + circ2.comments
+        comments=circ1.comments + circ2.comments
     )
