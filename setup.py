0d41675be28a749c2a1b25a8f0742a7b7957c87b
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-10-24 13:29:11 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ DESC = 'A python library for manipulating sequential and-inverter gates.'
 
 setup(
     name='py-aiger',
-    version='2.1.0',
+    version='2.1.1',
     description=DESC,
     url='http://github.com/mvcisback/py-aiger',
     author='Marcell Vazquez-Chanlatte',
