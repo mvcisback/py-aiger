45b9392807582e5c9574662dcf6bcec806c7df61
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-03-03 22:31:22 -0800

    Update __init__.py

--- aiger/__init__.py
+++ aiger/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from aiger.aig import AIG
 from aiger.common import and_gate, or_gate, bit_flipper, source, sink
-from aiger.common import tee, empty, identity, ite, delay, parity_gate
+from aiger.common import tee, empty, identity, delay, parity_gate
 from aiger.parser import parse, load
-from aiger.expr import atom, BoolExpr
+from aiger.expr import atom, ite, BoolExpr
